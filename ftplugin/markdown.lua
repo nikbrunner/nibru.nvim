802f82d62ddfea1d992a24137546b158a985161c
nbr <nikolaus.brunner@protonmail.ch>
2025-01-12 18:43:01 +0100

    ftplugin(markdown): enhance list handling and line wrapping
    - feat: add `continue_list` function for automatic list/checkbox continuation
    - feat: set wrap locally for markdown buffers
    - refactor: improve toggle_checkbox function with empty line handling
    - style: update cursor handling for better UX

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,5 +1,7 @@
 local map = vim.keymap.set
 
+vim.opt_local.wrap = true
+
 local function toggle_checkbox()
     local line = vim.api.nvim_get_current_line()
 
@@ -27,10 +29,59 @@ local function toggle_checkbox()
     end
 end
 
+local function continue_list()
+    local line = vim.api.nvim_get_current_line()
+    local cursor = vim.api.nvim_win_get_cursor(0)
+    local row = cursor[1]
+
+    -- Check for checkbox pattern first
+    local checkbox_pattern = "^%s*-%s%[.%]%s"
+    if line:match(checkbox_pattern) then
+        -- Get the indentation
+        local indent = line:match("^(%s*)")
+
+        -- If the current line is empty except for the checkbox, remove it
+        if line:match("^%s*-%s%[.%]%s*$") then
+            vim.api.nvim_buf_set_lines(0, row - 1, row, false, { "" })
+            return vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, false, true), "n", true)
+        end
+
+        -- Create a new unchecked checkbox
+        return vim.api.nvim_feedkeys(
+            vim.api.nvim_replace_termcodes("<CR>" .. indent .. "- [ ] ", true, false, true),
+            "n",
+            true
+        )
+    end
+
+    -- Check for regular list items (- or *)
+    local list_pattern = "^%s*[-*]%s"
+    if line:match(list_pattern) then
+        -- Get the list marker and its indentation
+        local indent = line:match("^(%s*)")
+        local marker = line:match("^%s*([-*])")
+
+        -- If the current line is empty except for the marker, remove it
+        if line:match("^%s*[-*]%s*$") then
+            vim.api.nvim_buf_set_lines(0, row - 1, row, false, { "" })
+            return vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, false, true), "n", true)
+        end
+
+        -- Create a new list item
+        return vim.api.nvim_feedkeys(
+            vim.api.nvim_replace_termcodes("<CR>" .. indent .. marker .. " ", true, false, true),
+            "n",
+            true
+        )
+    end
+
+    -- If not on a list item or checkbox, just return a normal Enter
+    return vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, false, true), "n", true)
+end
+
 map({ "n", "o", "x" }, "j", "gj", {})
 map({ "n", "o", "x" }, "k", "gk", {})
 map({ "n", "o", "x" }, "0", "g0", {})
 map({ "n", "o", "x" }, "$", "g$", {})
 map({ "n", "i" }, "<C-CR>", toggle_checkbox, { noremap = true, silent = true })
-
-vim.opt.wrap = true
+map({ "i" }, "<CR>", continue_list, { buffer = true, expr = false })
