0fcfba7f99effc6b77fd711763c489492654687b
nbr <nikolaus.brunner@protonmail.ch>
2025-01-12 18:30:07 +0100

    ftplugin(markdown): enhance checkbox toggle functionality in markdown files
    - refactor: change toggle mapping from `K` to `<C-CR>` for normal and insert modes
    - feat: add cursor positioning after creating new checkbox on empty line
    - feat: remove auto-checkbox creation for non-empty lines without checkboxes

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -5,26 +5,32 @@ local function toggle_checkbox()
 
     local checked_pattern = "%- %[x%]"
     local unchecked_pattern = "%- %[ %]"
+    local move_cursor_to_end = false
 
     if line:match(checked_pattern) then
         line = line:gsub(checked_pattern, "- [ ]")
     elseif line:match(unchecked_pattern) then
         line = line:gsub(unchecked_pattern, "- [x]")
-    else
-        -- If no checkbox exists, create an unchecked one at the start of the line
-        line = "- [ ] " .. line
-        -- TODO: Use A to append to the end of the line
+    elseif line:match("^%s*$") then
+        line = "- [ ] "
+        move_cursor_to_end = true
     end
 
     -- Set the modified line
     local row = vim.api.nvim_win_get_cursor(0)[1]
     vim.api.nvim_buf_set_lines(0, row - 1, row, false, { line })
+
+    -- Move cursor to end of line if needed
+    if move_cursor_to_end then
+        local line_length = #line
+        vim.api.nvim_win_set_cursor(0, { row, line_length })
+    end
 end
 
 map({ "n", "o", "x" }, "j", "gj", {})
 map({ "n", "o", "x" }, "k", "gk", {})
 map({ "n", "o", "x" }, "0", "g0", {})
 map({ "n", "o", "x" }, "$", "g$", {})
-map("n", "K", toggle_checkbox, { noremap = true, silent = true })
+map({ "n", "i" }, "<C-CR>", toggle_checkbox, { noremap = true, silent = true })
 
 vim.opt.wrap = true
