15418807dccd0dc7e79db5e4ab9d9e3bc2a37ada
nbr <nikolaus.brunner@protonmail.ch>
2025-01-16 17:39:36 +0100

    specs(colorschemes): add github theme

--- lua/nbr/specs/colorschemes.lua
+++ lua/nbr/specs/colorschemes.lua
@@ -20,6 +20,7 @@ return {
             },
         },
     },
+
     {
         "folke/tokyonight.nvim",
         lazy = false,
@@ -31,5 +32,38 @@ return {
                 highlights.YaziFloat = { link = "NormalFloat" }
             end,
         },
+    },
+
+    {
+        "projekt0n/github-nvim-theme",
+        lazy = false,
+        priority = 1000,
+        ---@type GhTheme.Config
+        opts = {
+            options = {
+                hide_end_of_buffer = true,
+                styles = {
+                    comments = "italic",
+                },
+                darken = {
+                    floats = true,
+                    sidebars = {
+                        enable = true,
+                    },
+                },
+            },
+            groups = {
+                all = {
+                    FzfLuaNormal = { bg = "bg0" },
+                    FzfLuaBorder = { bg = "bg0" },
+                    NormalFloat = { bg = "bg0" },
+                    FloatTitle = { bg = "bg0" },
+                    FloatBorder = { bg = "bg0" },
+                },
+            },
+        },
+        config = function(_, opts)
+            require("github-theme").setup(opts)
+        end,
     },
 }
