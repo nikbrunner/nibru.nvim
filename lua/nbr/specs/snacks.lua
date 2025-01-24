d1e5d771829f605dffb25eb2397f53ec720ad8e8
nbr <nikolaus.brunner@protonmail.ch>
2025-01-24 08:59:44 +0100

    feat(snacks): exclude current from buffers

--- lua/nbr/specs/snacks.lua
+++ lua/nbr/specs/snacks.lua
@@ -56,6 +56,9 @@ return {
             },
 
             sources = {
+                buffers = {
+                    current = false,
+                },
                 files = {
                     layout = {
                         preset = "vscode",
