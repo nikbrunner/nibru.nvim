c396482f5a1ed58034e995a178b3b8a5c1684b33
nbr <nikolaus.brunner@protonmail.ch>
2025-01-26 17:58:14 +0100

    feat(deps): upgrade and use default keymaps for blink

--- lua/nbr/specs/blink.lua
+++ lua/nbr/specs/blink.lua
@@ -6,20 +6,6 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-        keymap = {
-            ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-            ["<C-e>"] = { "hide" },
-            ["<C-y>"] = { "select_and_accept" },
-
-            ["<Up>"] = { "select_prev", "fallback" },
-            ["<Down>"] = { "select_next", "fallback" },
-
-            ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-            ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
-            ["<Tab>"] = { "snippet_forward", "fallback" },
-            ["<S-Tab>"] = { "snippet_backward", "fallback" },
-        },
         sources = {
             default = { "lazydev", "lsp", "path", "snippets", "buffer" },
             providers = {
